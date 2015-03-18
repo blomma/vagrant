48bbc238973eeaf852a8df5dbe7459d5518fb881
Mikael Hultgren <blomma@gmail.com>
2015-03-18 15:40:29 +0100

    Added filezilla

--- scripts/dev/Install-Apps.ps1
+++ scripts/dev/Install-Apps.ps1
@@ -4,6 +4,7 @@ cinst -y vs2013.4
 cinst -y resharper -Version 8.2.3000.5176
 cinst -y linqpad
 cinst -y notepadplusplus.install
+cinst -y filezilla
 
 wget https://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/44/WebEssentials2013.vsix -OutFile C:\Users\vagrant\WebEssentials2013.vsix | out-null
 & 'C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\VSIXInstaller.exe' /q C:\Users\vagrant\WebEssentials2013.vsix | out-null
