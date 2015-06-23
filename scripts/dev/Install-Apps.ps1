ba6b503b5a06f7419403f318b4ab62b6bacb3e41
Mikael Hultgren <blomma@gmail.com>
2015-06-23 10:19:18 +0200

    Add libreoffice

--- scripts/dev/Install-Apps.ps1
+++ scripts/dev/Install-Apps.ps1
@@ -6,6 +6,7 @@ cinst -y linqpad
 cinst -y notepadplusplus.install
 cinst -y filezilla
 cinst -y tortoisesvn
+cinst -y libreoffice
 
 wget https://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/44/WebEssentials2013.vsix -OutFile C:\Users\vagrant\WebEssentials2013.vsix | out-null
 & 'C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\VSIXInstaller.exe' /q C:\Users\vagrant\WebEssentials2013.vsix | out-null
